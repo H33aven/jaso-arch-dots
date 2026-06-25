0ff139db5cab280a9e7dc5e830d7d42a21f783a4
Jasō <192628995+H33aven@users.noreply.github.com>
2026-06-25 22:48:08 +0000

    Add files via upload

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -1,14 +1,6 @@
---require("modules.variables")
 require("modules.colors")
 require("modules.monitors")
 require("modules.execs")
 require("modules.general")
 require("modules.binds")
 require("modules.rules")
-require("modules.env")
---exec-once = swaybg -i /home/jaso/Wallpapers/girls.jpg -m fill &
---exec-once = hyprctl setcursor Bibata-Modern-Classic 20
---exec-once = gentoo-pipewire-launcher restart
-
---mtc ~/Wallpapers/girls.jpg --source-color-index 0
---sudo emerge --ask app-misc/illogical-impulse-{audio,backlight,basic,bibata-modern-classic-bin,kde,microtex-git,portal,python,screencapture,toolkit,widgets} --autounmask
--- .config/hypr/modules/colors.lua
+++ .config/hypr/modules/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border = "rgba(8f909677)",
-            inactive_border = "rgba(45474c55)",
+            active_border = "rgba(9b8d9177)",
+            inactive_border = "rgba(4f444855)",
         }
     },
     misc = {
-        background_color = "rgba(131314FF)",
+        background_color = "rgba(161214FF)",
     }
 })
 
 hl.window_rule({
     match = { pin = 1 },
-    border_color = "rgba(c0c6d9AA)"
+    border_color = "rgba(eec1d3AA)"
 })
